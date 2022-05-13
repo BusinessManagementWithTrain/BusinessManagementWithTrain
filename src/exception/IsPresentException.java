639422d2adc839700487adf33fb533c193c2882a
L0P0P <grassoemanuele@live.com>
2022-05-14 00:19:56 +0200

    Primo upload eccezioni

---
+++ src/exception/IsPresentException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class IsPresentException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public IsPresentException() {
+		// TODO Auto-generated constructor stub
+	}
+}
