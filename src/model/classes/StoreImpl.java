5c21415003a1dbeaf6a8e7eaa43b2cfbf8dbf651
highscara <eliascaramuzzino@gmail.com>
2022-05-15 16:02:26 +0200

    caricamento singleton pattern e modifica magazzino

--- src/model/classes/StoreImpl.java
+++ src/model/classes/StoreImpl.java
@@ -21,12 +21,15 @@ public class StoreImpl implements Store {
 	 */
 	private Warehouse loadingWarehouse;	
 	
+
+	
 	/**
 	 * Il costruttore servirà unicamente per riscrivere il funzionamento del magazzino di carico
 	 * dal quale verranno tolti tutti i controlli e non verrà utilizzato neanche un metodo, questo
 	 * ci consentirà di utilizzare il negozio come un'azienda ma che all'effettivo non farà nulla.
+	 * Il costruttore sarà privato per consentire l'utilizzo del Singleton Pattern
 	 */
-	public StoreImpl() {
+	private StoreImpl() {
 		this.loadingWarehouse = new Warehouse(){
 			@Override
 			public void addMaterial(int quantity) {}
@@ -42,9 +45,34 @@ public class StoreImpl implements Store {
 			@Override
 			public int getCurrentCapacity() {
 				return 0;
+			}
+
+			@Override
+			public String getMaterial() {
+				return null;
 			}	
 		};	
 	}
+	
+	/*
+	 * Classe dedicata all'utilizzo del Singleton Pattern, questa classe contiene solo una costante.
+	 * La scelta di questo stile risiede nelli0deologia dei puntare ad una soluzione comoda non solo
+	 * nell'implementazione, ma anche a livello hardware;
+	 * il riferimento alla classe StoreImpl e l'allocazione della stessa all'interno della memoria
+	 * avverrà solo dopo la prima invocazione del metodo getStore()
+	 */
+	private static class SingletonHelper {
+        private static final StoreImpl STORE = new StoreImpl();
+    }
+
+	/*
+	 * Consente di avere il riferimento sempre alla stessa istanza della classe StoreImpl
+	 * 
+	 * @return l'istanza della classe StoreImpl
+	 */
+    public static StoreImpl getStoreInstance() {
+        return SingletonHelper.STORE;
+    }
 	
 	/*
 	 * Metodo che consente di avere il riferimento al nome dell'azienda
