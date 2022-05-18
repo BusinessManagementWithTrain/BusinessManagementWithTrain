26218c7b75f92030f6ecc04d382198ea161d795b
simorina <rinaldisimone8@gmail.com>
2022-05-18 16:42:40 +0200

    update clase train, inizio view

--- src/view/train/TrainFrame.java
+++ src/view/train/TrainFrame.java
@@ -1,6 +1,7 @@
 package view.train;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * 
@@ -11,5 +12,13 @@ import javax.swing.JFrame;
 
 public class TrainFrame extends JFrame{
 	private static final long serialVersionUID = 1L;
+	
+	/*inizio view*/
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	
+	
+	
 
 }
